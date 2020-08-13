96754421693d0f4330bacce645cf1e6dd48e213f
Shivix <52997365+Shivix@users.noreply.github.com>
2020-08-13 22:32:45 +0100

    added commandBuffer class

---
+++ include/commandBuffer.hpp
@@ -0,0 +1,19 @@
+#ifndef VULKANTRIANGLE_COMMANDBUFFER_HPP
+#define VULKANTRIANGLE_COMMANDBUFFER_HPP
+
+
+class commandBuffer{
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
+
+
+#endif //VULKANTRIANGLE_COMMANDBUFFER_HPP
